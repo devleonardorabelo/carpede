fcf051251022769f08ebf41295cc6f4609a8b586
deleonardorabelo <leonardomrabelo@gmail.com>
2020-07-24 23:20:46 -0300

    changed Store Models/ Profile Controller

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -5,28 +5,36 @@ module.exports = {
         
         const store = req.headers.user;
 
-        const { avatar, name, description, whatsapp, phone, tags } = await Store.findOne({_id: store.id});
+        const { avatar, name, whatsapp, phone, operation, averageDeliveryTime } = await Store.findOne({_id: store.id});
 
-        return res.json({ avatar, name, description, whatsapp, phone, tags })
+        return res.json({ avatar, name, whatsapp, phone, operation, averageDeliveryTime })
 
     },
     async update(req, res) {
 
         const store = req.headers.user;
 
-        const { avatar, name, description, whatsapp, phone, tags } = req.body;
+        const { avatar, name, whatsapp, phone, operation, averageDeliveryTime } = req.body;
 
         if(!name) return res.json({ error: { text: 'Mínimo de 5 caracteres', input: 'name' } });
         
-        if(!whatsapp) return res.json({ error: { text: 'Ex: 01 2 3456 7890', input: 'whatsapp' } });
+        if(!whatsapp) return res.json({ error: { text: 'Ex: (01) 2 3456 7890', input: 'whatsapp' } });
 
+        if(whatsapp.length < 11) return res.json({ error: { text: '(01) 2 3456 7890', input: 'whatsapp' } });
+
+        if(operation.opening.length !== 5) return res.json({ error: { text: 'Incompleto', input: 'opening' } });
+
+        if(operation.closure.length !== 5) return res.json({ error: { text: 'Incompleto', input: 'closure' } });
+
+        if(!averageDeliveryTime) return res.json({ error: { text: 'Dê um valor', input: 'average' } });
+        
         await Store.updateOne({ _id: store.id }, {
             avatar,
             name,
-            description,
             whatsapp,
             phone,
-            tags
+            operation,
+            averageDeliveryTime
         });
 
         return res.json({
--- src/models/Store.js
+++ src/models/Store.js
@@ -3,13 +3,33 @@ const mongoose = require('mongoose');
 const StoreSchema = new mongoose.Schema({
     avatar: String,
     name: String,
-    description: String,
     whatsapp: String,
     phone: String,
-    tags: String,
     email: String,
     password: String,
     firebaseTokenNotification: String,
+    operation: {
+        opening:  {
+            type: String,
+            default: '18:00',
+        },
+        closure: {
+            type: String,
+            default: '00:00',
+        },
+    },
+    fees: {
+        payment: {
+            type: Number,
+            default: 0,
+        },
+        delivery: {
+            type: Number,
+            default: 0,
+        },
+    },
+    averageDeliveryTime: String,
+
 });
 
 module.exports = mongoose.model('Store', StoreSchema);
