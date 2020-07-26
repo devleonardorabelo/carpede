1d81ef294e916d82ef80915d64cd818a6db6173b
deleonardorabelo <leonardomrabelo@gmail.com>
2020-07-26 12:12:49 -0300

    fixed operation check

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -50,7 +50,7 @@ module.exports = {
     },
     async signup(req, res) {
 
-			const { avatar, name, whatsapp, email, password } = req.body;
+			const { avatar, name, whatsapp, email, password, operation, averageDeliveryTime } = req.body;
 
 			const mask = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 
@@ -73,7 +73,13 @@ module.exports = {
 			if(!whatsapp || whatsapp.length < 10) return res.json({ error: { text: 'Ex: 01 2 3456 7890', input: 'whatsapp' } });
 			if(!email || email.length < 13 || emailTest === false) return res.json({ error: { text: 'Verifique o email preenchido', input: 'email' } });
 			if(!password || password.length < 8) return res.json({ error: { text: 'Senha muito curta, mínimo de 8 caracteres', input: 'password' } });
-			
+			if(!operation.opening) return res.json({ error: { text: 'Preencha', input: 'opening' } });
+			if(!operation.closure) return res.json({ error: { text: 'Preencha', input: 'closure' } });
+			if(operation.opening >= operation.closure) return res.json({ error: { text: 'Deve ser maior que a abertura', input: 'closure' } });
+			if(operation.opening.length !== 5) return res.json({ error: { text: 'Incompleto', input: 'opening' } });
+			if(operation.closure.length !== 5) return res.json({ error: { text: 'Incompleto', input: 'closure' } });
+
+			if(!averageDeliveryTime) return res.json({ error: { text: 'Dê um valor', input: 'average' } });
 
 			let storeSchema = {
 				avatar,
--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -17,15 +17,13 @@ module.exports = {
         const { avatar, name, whatsapp, phone, operation, averageDeliveryTime } = req.body;
 
         if(!name) return res.json({ error: { text: 'Mínimo de 5 caracteres', input: 'name' } });
-        
         if(!whatsapp) return res.json({ error: { text: 'Ex: (01) 2 3456 7890', input: 'whatsapp' } });
-
         if(whatsapp.length < 11) return res.json({ error: { text: '(01) 2 3456 7890', input: 'whatsapp' } });
-
+        if(!operation.opening) return res.json({ error: { text: 'Preencha', input: 'opening' } });
+		if(!operation.closure) return res.json({ error: { text: 'Preencha', input: 'closure' } });
+		if(operation.opening >= operation.closure) return res.json({ error: { text: 'Deve ser maior que a abertura', input: 'closure' } });
         if(operation.opening.length !== 5) return res.json({ error: { text: 'Incompleto', input: 'opening' } });
-
         if(operation.closure.length !== 5) return res.json({ error: { text: 'Incompleto', input: 'closure' } });
-
         if(!averageDeliveryTime) return res.json({ error: { text: 'Dê um valor', input: 'average' } });
         
         await Store.updateOne({ _id: store.id }, {
